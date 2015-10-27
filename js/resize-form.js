836fa22d9f48c0950fda1da2c95d09a53aab696d
Keks <keks@htmlacademy.ru>
2015-10-27 20:21:50 +0300

    :cat: Обновления от Кекса для модуля «Медиа и графика»

--- js/resize-form.js
+++ js/resize-form.js
@@ -1,5 +1,3 @@
-'use strict';
-
 (function() {
   var uploadForm = document.forms['upload-select-image'];
   var resizeForm = document.forms['upload-resize'];
@@ -7,32 +5,6 @@
 
   var previewImage = resizeForm.querySelector('.resize-image-preview');
   var prevButton = resizeForm['resize-prev'];
-  var resizeX = resizeForm['resize-x'];
-  var resizeY = resizeForm['resize-y'];
-  var resizeSize = resizeForm['resize-size'];
-  var MINIMAL_SIDE_VALUE = 50;
-
-  resizeX.min = 0;
-  resizeY.min = 0;
-  resizeSize.min = MINIMAL_SIDE_VALUE;
-  resizeX.required = true;
-  resizeY.required = true;
-  resizeSize.required = true;
-
-  previewImage.onload = function() {
-    var imageWidth = previewImage.clientWidth;
-    var imageHeight = previewImage.clientHeight;
-    var imageMinimalSide = Math.min(imageWidth, imageHeight);
-
-    resizeX.max = imageMinimalSide - 1;
-    resizeY.max = resizeX.max;
-    resizeSize.max = imageMinimalSide - parseInt(resizeX.value, 10);
-
-    var elementToListen = imageMinimalSide === imageWidth ? resizeX : resizeY;
-    elementToListen.onchange = function() {
-      resizeSize.max = imageMinimalSide - parseInt(elementToListen.value, 10);
-    };
-  };
 
   prevButton.onclick = function(evt) {
     evt.preventDefault();
@@ -46,7 +18,6 @@
   resizeForm.onsubmit = function(evt) {
     evt.preventDefault();
     filterForm.elements['filter-image-src'] = previewImage.src;
-
 
     resizeForm.classList.add('invisible');
     filterForm.classList.remove('invisible');
